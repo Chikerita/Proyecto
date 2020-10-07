4ec2a6e63a7791a8cfebaafdef9da76008f00b31
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-07 18:05:59 -0500

    Menu Principal y menu de opciones
    Se creo el menu principal y el menu de opciones

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevelOne(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
