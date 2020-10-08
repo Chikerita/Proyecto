0e9246e749680590ba9c21b9693f2fa506a54c67
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-08 17:51:53 -0500

    Creacion de una simulacion
    Se creo la simulacion de movimiento parabolico

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/BulletController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public Canvas UI;
+    private float angle;
+    private float initialVelocity;
+    private float maxHeight;
+    private float maxDistance;
+    private float flightTime;
+    private bool flying;
+
+    // Start is called before the first frame update
+    void Start(){
+        angle = 45f;
+        initialVelocity = 20f;
+        maxHeight = 0f;
+        maxDistance = 0f;
+        flightTime = 0f;
+        flying = false;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (flying){
+            maxHeight = Mathf.Max(maxHeight, gameObject.transform.localPosition.y);
+            maxDistance = Mathf.Max(maxDistance, gameObject.transform.localPosition.x);
+            UI.GetComponent<UIController>().updateStats(maxHeight, maxDistance);
+        }
+    }
+
+    //Shoots the bullet whit the proper angle and initial velocity
+    public void shoot(){
+        float xComponent = Mathf.Cos((angle * Mathf.PI) / 180) * initialVelocity;
+        float yComponent = Mathf.Sin((angle * Mathf.PI) / 180) * initialVelocity;
+        Vector3 vector = new Vector3(xComponent, yComponent, 0);
+        gameObject.GetComponent<Rigidbody>().AddForce(vector, ForceMode.VelocityChange);
+        flying = true;
+    }
+
+    public void resetSimulation(){
+        UI.GetComponent<UIController>().resetStats();
+        gameObject.transform.localPosition = new Vector3(5.5f, 0.6f, 9f);
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        flying = false;
+        maxHeight = 0f;
+        maxDistance = 0f;
+        flightTime = 0f;
+    }
+
+    //Updates the angle when it's changed from the slider or the input
+    public void updateAngle(float value){
+        angle = value;
+    }
+
+    //Updates the initial velocity when it's changed from the slider or the input
+    public void updateVelocity(float value){
+        initialVelocity = value;
+    }
+
+}
---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/UIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour{
+
+    public InputField angleInput;
+    public Slider angleSlider;
+    public InputField velInput;
+    public Slider velSlider;
+    public Text statsLabel;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void updateAngleInput(float value){
+        angleInput.text = value.ToString();
+    }
+
+    public void updateAngleSlider(string text){
+        angleSlider.value = float.Parse(text);
+    }
+
+    public void updateVelocityInput(float value){
+        velInput.text = value.ToString();
+    }
+
+    public void updateVelocitySlider(string text){
+        velSlider.value = float.Parse(text);
+    }
+
+    public void updateStats(float yPosition, float xPosition){
+        statsLabel.text = string.Format("Ymax: {0}m\nXmax: {1}m\nFlight time: {2}s", yPosition.ToString("F3"), xPosition.ToString("F3"), "0");
+    }
+
+    public void resetStats(){
+        statsLabel.text = "Ymax: 0m\nXmax: 0m\nFlight time: 0s";
+    }
+
+}
