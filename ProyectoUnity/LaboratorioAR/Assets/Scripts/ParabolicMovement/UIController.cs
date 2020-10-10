f11f59e00e2c46a0e7576cfe4e3dbc13be80cadd
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-10 17:37:30 -0500

    Controles del obstaculo
    Se agregaron los controles del obstaculo

--- ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/UIController.cs
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/UIController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour{
@@ -19,6 +17,10 @@ public class UIController : MonoBehaviour{
     // Update is called once per frame
     void Update() {
         
+    }
+
+    public void zzzzzzzz(InputField input, float value){
+        input.text = "20";
     }
 
     public void updateAngleInput(float value){
--- ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/WallController.cs
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/WallController.cs
@@ -1,18 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class WallController : MonoBehaviour {
-        
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update(){
-        
-    }
 
     public void changeHeight(float value){
         transform.position = new Vector3(transform.position.x, value - 15.1f, transform.position.z);
