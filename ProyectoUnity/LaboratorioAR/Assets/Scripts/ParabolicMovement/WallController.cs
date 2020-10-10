500000bc14dc2f0b647a5f02d9eb6120139987e0
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-09 20:20:53 -0500

    Actualizacion de la simulacion de tiro parabolico
    Se agrego un obstaculo en la simulacion

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/WallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+        
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void changeHeight(float value){
+        transform.position = new Vector3(transform.position.x, value - 15.1f, transform.position.z);
+    }
+
+    public void changeDistance(float value){
+        transform.position = new Vector3(value + 6f, transform.position.y, transform.position.z);
+    }
+}
