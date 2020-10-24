ba33269b9cea834fcab4591ceb01515bcbde3b7f
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-23 21:21:35 -0500

    Menu simulaciones
    Se agrego el menu que muetra las simulaciones

--- ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/MainMenuController.cs
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
-{
+public class MainMenuController : MonoBehaviour {
+
+    private string[] simulations = {"ParabolicMovement"};
+    public Transform menuPanel;
+    public GameObject buttonPrefab;
 
     void Start(){
         Screen.orientation = ScreenOrientation.Portrait;
+		for (int i = 0; i < simulations.Length; i++){
+			GameObject button = (GameObject) Instantiate (buttonPrefab);
+			button.GetComponentInChildren<Text>().text = simulations[i];
+			int index = i;
+			button.GetComponent<Button>().onClick.AddListener(() => {
+                SceneManager.LoadScene(button.GetComponentInChildren<Text>().text);
+            });
+			button.transform.SetParent(menuPanel, false);
+		}
     }
     
     // Update is called once per frame
