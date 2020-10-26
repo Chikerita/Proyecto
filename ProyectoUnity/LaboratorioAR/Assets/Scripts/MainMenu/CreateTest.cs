ab7741d57e566eb6613586986bada349eef7e583
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-25 20:45:25 -0500

    Cambios al menu de cuestionarios

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/CreateTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CreateTest : MonoBehaviour {
+
+    public GameObject pmFormPrefab;
+    public Transform listViewContent;
+
+    public void newPMQuestion(){
+        GameObject form = (GameObject) Instantiate (pmFormPrefab);
+        form.transform.SetParent(listViewContent, false);
+        foreach (Transform child in listViewContent.transform){
+            Debug.Log(child.name);
+        }
+    }
+
+}
