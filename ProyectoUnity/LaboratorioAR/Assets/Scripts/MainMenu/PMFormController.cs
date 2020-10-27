813ffaa38bacdb46e6216398b6eec91ab75ed593
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-10-26 20:20:28 -0500

    Funcionalidad a la creacion de cuestionarios
    Se agrego funcionalidad al menu de creacion de cuestionarios, generan un json del cuestionario y las preguntas

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/PMFormController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class PMFormController : MonoBehaviour {
+    
+    public Dropdown type;
+    public InputField angle;
+    public InputField velocity;
+    public InputField targetDistance;
+    public InputField wallHeight;
+    public InputField wallDistance;
+
+    public string generateJSON(){
+        string type = this.type.options[this.type.value].text;
+        float angle = float.Parse(this.angle.text);
+        float velocity = float.Parse(this.velocity.text);
+        float targetDistance = float.Parse(this.targetDistance.text);
+        float wallHeight = float.Parse(this.wallHeight.text);
+        float wallDistance = float.Parse(this.wallDistance.text);
+        PMQuestion question = new PMQuestion(type, angle, velocity, targetDistance, wallHeight, wallDistance, 9.8f);
+        string temp = JsonUtility.ToJson(question);
+        Debug.Log(temp);
+        return temp;
+    }
+
+    public void delete(){
+        GameObject.Destroy(gameObject);
+    }
+}
--- ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/PMQuestion.cs
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/PMQuestion.cs
@@ -6,14 +6,32 @@ public class PMQuestion {
     public const string GRAVITY_TYPE = "gravity";
     public const string WALL_TYPE = "wall";
     private const string SIMULATION = "ParabolicMovement";
-    private string type;
-    private string statement;
-    private float angle;
-    private float velocity;
-    private float maxDistance;
-    private float maxHeight;
-    private float midlePoint;
-    private float gravity;
+    public string type;
+    public string statement;
+    public float angle;
+    public float velocity;
+    public float maxDistance;
+    public float maxHeight;
+    public float midlePoint;
+    public float gravity;
+
+    public PMQuestion(string type, float angle, float velocity, float maxDistance, float maxHeight, float midlePoint, float gravity){
+        this.type = type;
+        this.angle = angle;
+        this.velocity = velocity;
+        this.maxDistance = maxDistance;
+        this.maxHeight = maxHeight;
+        this.midlePoint = midlePoint;
+        this.gravity = gravity;
+        if (type.CompareTo(CANNON_TYPE) == 0){
+            statement = string.Format("Prepare el cañon para disparar un proyectil que pase por encima de la pared que mide {0}m y esta a {1}m, y ,"+
+                                        "golpee el objetivo que se encuentra a {2}m", maxHeight, midlePoint, maxDistance);
+        } else {
+            statement = string.Format("El cañon esta dispuesto para disparar a una velociadad de {0} a un angulo de {1},"+
+                                        "coloque el objetivo en la distancia maxima y la pared debajo de la altura maxima", velocity, angle);
+        }
+    }
+
 
     public PMQuestion(){
         if(Random.Range(0f, 1f) > 0.5f){
@@ -38,6 +56,7 @@ public class PMQuestion {
                                         "coloque el objetivo en la distancia maxima y la pared debajo de la altura maxima", velocity, angle);
         }
     }
+
 
     public string Type { get => type; set => type = value; }
     public string Statement { get => statement; set => statement = value; }
