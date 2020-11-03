21a88945366291630c909f1a6dd10908a988d9ba
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-03 01:04:51 -0500

    Creacion del menu de cuestionarios

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/QuestionaryMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class QuestionaryMenu : MonoBehaviour {
+    void OnEnable(){
+        StartCoroutine(GetText());
+    }
+ 
+    IEnumerator GetText() {
+        UnityWebRequest req = UnityWebRequest.Get("http://localhost:3000/get");
+        req.SetRequestHeader("Content-Type", "application/json");
+        yield return req.SendWebRequest();
+        if(req.isNetworkError || req.isHttpError) {
+            Debug.Log(req.error);
+        } else {
+            Debug.Log(req.downloadHandler.text);
+        }
+    }
+}
