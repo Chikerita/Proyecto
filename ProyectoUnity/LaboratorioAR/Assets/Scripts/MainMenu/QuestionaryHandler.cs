16a15e64da48ca5f497380f438f219f5ffe468f8
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-05 00:16:14 -0500

    Cambios al menu principal

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/MainMenu/QuestionaryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionaryHandler : MonoBehaviour {
+    
+    public static Questionary questionary;
+
+    public void startQuestionary(){
+        
+    }
+
+    void Update() {
+        
+    }
+}
