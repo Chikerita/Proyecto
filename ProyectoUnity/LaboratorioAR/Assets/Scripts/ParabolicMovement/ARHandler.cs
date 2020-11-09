25f9d8bf5e72b5741835b6ca6288bec49cf3cfab
Chikerita <48142553+Chikerita@users.noreply.github.com>
2020-11-08 21:14:54 -0500

    Actualizacion Vuforia

---
+++ ProyectoUnity/LaboratorioAR/Assets/Scripts/ParabolicMovement/ARHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARHandler : MonoBehaviour {
+
+    public GameObject objects;
+    public GameObject arCamera;
+    public GameObject normalCamera;
+    public GameObject stage;
+
+    public void changeCamera(){
+        if(arCamera.activeInHierarchy)activateNormal();
+        else activateAR();
+    }
+    
+    private void activateAR(){
+        normalCamera.SetActive(false);
+        arCamera.SetActiveRecursively(true);
+        stage.SetActive(true);
+        objects.transform.SetParent(stage.transform, false);
+    }
+
+    private void activateNormal(){
+        objects.transform.SetParent(null, false);
+        arCamera.SetActiveRecursively(false);
+        stage.SetActive(false);
+        normalCamera.SetActive(true);
+    }
+}
